3e802acb7f5431ba3ea02afcaa66c06dfce4b7b6
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2025-03-28 12:56:21 -0300

    Feat: test deploy

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"budget-service/database"
 	"budget-service/routes"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +17,7 @@ func main() {
 
 	// Definir rotas
 	routes.SetupRoutes(router)
-
+	fmt.Println("Server running on port: 8082")
 	// Rodar a aplicação na porta 8082
 	router.Run(":8082")
 }
