ce91ca9a471bf5458b22b88e63c055c77c1ea988
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2025-03-28 14:59:13 -0300

    fix: .env

--- database/db.go
+++ database/db.go
@@ -5,34 +5,29 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-// Função para conectar ao banco de dados PostgreSQL
 func ConnectDatabase() {
-	// Carregar as variáveis do arquivo .env
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Erro ao carregar o arquivo .env", err)
-	}
-
-	// Obter a URL do banco de dados do arquivo .env
+	// Obtém a URL do banco de dados da variável de ambiente
 	dsn := os.Getenv("DATABASE_URL")
+
+	// Verifica se a variável de ambiente está vazia
 	if dsn == "" {
-		log.Fatal("DATABASE_URL não encontrada no arquivo .env")
+		log.Fatal("DATABASE_URL não encontrada. Certifique-se de que a variável de ambiente está configurada no servidor.")
 	}
 
-	// Conectar ao banco de dados PostgreSQL usando a variável de ambiente
+	// Conecta ao PostgreSQL
+	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Falha ao conectar ao banco de dados:", err)
 	}
 
-	// Usando AutoMigrate para criar a tabela se ela não existir
+	// Realiza AutoMigrate
 	err = DB.AutoMigrate(&models.Budget{})
 	if err != nil {
 		log.Fatalf("Erro ao realizar AutoMigrate: %v", err)
