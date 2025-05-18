fae1de44f093dbc362d272baacde85335472119e
Dhione Castilho <dhione.cb@gmail.com>
2025-05-18 14:38:01 -0300

    feat: add cors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,9 @@ import (
 	"budget-service/internal/budget"
 	"budget-service/internal/database"
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,12 +16,25 @@ func main() {
 
 	// Inicializa e configura o servidor
 	router := gin.Default()
-	router.MaxMultipartMemory = 100 << 20 // 64MB
+
+	// ✅ Middleware de CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // ajuste para produção depois
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Configura limite de upload de arquivos
+	router.MaxMultipartMemory = 100 << 20 // 100 MB
+
+	// Registra as rotas da API
 	budget.SetupRoutes(router)
 
 	log.Println("✅ Servidor rodando na porta 8082")
-	err := router.Run(":8082")
-	if err != nil {
+	if err := router.Run(":8082"); err != nil {
 		log.Fatalf("❌ Erro ao iniciar o servidor: %v", err)
 	}
 }
